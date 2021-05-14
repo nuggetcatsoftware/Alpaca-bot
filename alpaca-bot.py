69aee8fb52b7177e7178872a5f70ede57898140e
nuggetcatsoftware <nuggetcatsoftware@gmail.com>
2021-05-14 20:39:28 +0800

    Bug fixes

--- alpaca-bot.py
+++ alpaca-bot.py
@@ -149,6 +149,7 @@ async def youtube(ctx, search):
     # print(html_content.read().decode())
     search_results = re.findall('href=\"\\/watch\\?v=(.{11})', html_content.read().decode())
     print(search_results)
+    await ctx.send(search_results)
     # I will put just the first result, you can loop the response to show more results
     await ctx.send('https://www.youtube.com/watch?v=' + search_results[0])
 @bot.command(name='join')
